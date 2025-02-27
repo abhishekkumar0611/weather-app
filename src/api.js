232afb6e393d17b784496a3f1cddf40a026b2259
AbhishekKumar Demo <abhishekkumar1106@gmail.in>
2025-02-27 16:56:54 +0530

    first commit in weather project

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'a206e1471bmsh3b7a2135cd41066p185e44jsna3c0fd3af4be',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "52c60a0e3fe6ab552e5163068fe42638"
